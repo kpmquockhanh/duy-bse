d56c276abaa147a948172fdf48811b65d9a00aaf
khanhlq <khanhlq@hblab.n>
2020-02-10 20:49:29 +0700

    Add contact form 7

--- app/wp-content/themes/duy/footer-content.php
+++ app/wp-content/themes/duy/footer-content.php
@@ -15,54 +15,8 @@
 										tiết về dự án: mặt bằng, bảng giá, chương trình mở bán và thăm quan căn hộ
 										mẫu … qua thông tin email hay số điện thoại mà Anh/Chị cung cấp</p>
 
-									<div role="form" class="wpcf7" id="wpcf7-f37-o1" lang="vi" dir="ltr">
-										<div class="screen-reader-response"></div>
-										<form action="/#wpcf7-f37-o1" method="post" class="wpcf7-form"
-											  novalidate="novalidate">
-											<div style="display: none;">
-												<input type="hidden" name="_wpcf7" value="37"/>
-												<input type="hidden" name="_wpcf7_version" value="5.1.4"/>
-												<input type="hidden" name="_wpcf7_locale" value="vi"/>
-												<input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f37-o1"/>
-												<input type="hidden" name="_wpcf7_container_post" value="0"/>
-											</div>
-											<div class="row">
-												<div class="col-xs-12 col-sm-6">
-													<div class="list_group"><span
-																class="wpcf7-form-control-wrap your-name"><input
-																	type="text" name="your-name" value="" size="40"
-																	class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
-																	aria-required="true" aria-invalid="false"
-																	placeholder="Họ và tên"/></span></div>
-													<div class="list_group"><span
-																class="wpcf7-form-control-wrap tel-567"><input
-																	type="tel" name="tel-567" value="" size="40"
-																	class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel"
-																	aria-required="true" aria-invalid="false"
-																	placeholder="Số điện thoại"/></span></div>
-													<div class="list_group"><span
-																class="wpcf7-form-control-wrap your-email"><input
-																	type="email" name="your-email" value=""
-																	size="40"
-																	class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email"
-																	aria-required="true" aria-invalid="false"
-																	placeholder="Địa chỉ email"/></span></div>
-												</div>
-												<div class="col-xs-12 col-sm-6">
-													<div class="list_group"><span
-																class="wpcf7-form-control-wrap your-message"><textarea
-																	name="your-message" cols="40" rows="10"
-																	class="wpcf7-form-control wpcf7-textarea"
-																	aria-invalid="false"
-																	placeholder="Nội dung"></textarea></span></div>
-												</div>
-											</div>
-											<div class="list_group"><input type="submit" value="Gửi yêu cầu"
-																		   class="wpcf7-form-control wpcf7-submit"/>
-											</div>
-											<div class="wpcf7-response-output wpcf7-display-none"></div>
-										</form>
-									</div>
+									<?= do_shortcode('[contact-form-7 id="52" title="Contact"]') ?>
+
 								</div>
 							</div>
 						</div>
